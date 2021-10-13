a6bdb97b5f266f0cbbb7083b09258c88be999c0c
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-10-12 20:28:03 -0400

    Fixes to include two routers, indexRouter for a homepage, and teamRouter that returns all team data

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const path = require("path");
 
-const teamRouter = require("./routes/team");
+const teamRouter = require("./routes/teamRouter");
+const indexRouter = require("./routes/indexRouter");
 
 const app = express();
 
@@ -15,11 +16,8 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-console.log(teamRouter)
-
+app.use('/', indexRouter);
 app.use("/api/team", teamRouter)
-
-
 
 app.listen(3000, function(){
    console.log(`Server is running on PORT: ${3000}`);
